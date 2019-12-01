03fe1d83423de2ba02fc29f7dcbfdab57e390397
cloud-native-tokyo <qiyuqi1995@gmail.com>
2019-12-01 19:36:40 +0900

    fix google analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-152891383-2",
+        trackingId: "UA-152891383-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
